254ec9d9c3c70657258c57d51938902cbd5a1acc
supra <rikc.2000@gmail.com>
2023-11-07 18:13:57 -0500

    temp

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import Hello from "./hello.js";
@@ -12,7 +13,12 @@ const app = express();
 app.use(express.json()); //parsing http data
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,6 +35,6 @@ app.use("/api/courses", courseRoutes);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("server started on port 4000");
 });
