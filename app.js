2c0c90a0bbcc8f62a23c1ef8c684e3ca9e071fed
supra <rikc.2000@gmail.com>
2023-12-04 18:01:28 -0500

    try

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use((req, res, next) => {
-  //   res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
